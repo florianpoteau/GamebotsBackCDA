691ef0390a616c9e46c99d335cde7324c61620f1
YoanBor <yoanbor@icloud.com>
2024-04-04 16:20:09 +0200

    feat(conversation): Ajout de l'entity Conversation

--- src/main/java/co/simplon/gamebotsback/persistance/entity/Conversation.java
+++ src/main/java/co/simplon/gamebotsback/persistance/entity/Conversation.java
@@ -1,4 +1,59 @@
 package co.simplon.gamebotsback.persistance.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "conversations")
 public class Conversation {
+
+    @Id
+    @Column(name = "id_conversations")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idConversations;
+
+    @Basic
+    @Column(name = "name")
+    private String name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modification_date")
+    private Date modificationDate;
+
+    public int getIdConversations() {
+        return idConversations;
+    }
+
+    public void setIdConversations(int idConversations) {
+        this.idConversations = idConversations;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
 }
