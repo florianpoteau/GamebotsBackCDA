dab8f2af4826a279d652166b4dc25485c6e4a28d
YoanBor <yoanbor@icloud.com>
2024-04-04 15:27:25 +0200

    feat(message): Ajout de l'entity Message

--- src/main/java/co/simplon/gamebotsback/persistance/entity/Message.java
+++ src/main/java/co/simplon/gamebotsback/persistance/entity/Message.java
@@ -1,4 +1,71 @@
 package co.simplon.gamebotsback.persistance.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    @Id
+    @Column(name = "id_messages")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMessages;
+
+    @Basic
+    @Column(name = "content")
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modification_date")
+    private Date modificationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "id_conversations")
+    private Conversation conversation;
+
+    public int getIdMessages() {
+        return idMessages;
+    }
+
+    public void setIdMessages(int idMessages) {
+        this.idMessages = idMessages;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
 }
