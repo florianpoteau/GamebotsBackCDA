4ac9df0ba87a2b24cde9290c63e8cdc03792c97b
YoanBor <yoanbor@icloud.com>
2024-04-04 15:24:41 +0200

    feat(user): Ajout de l'entity User

--- src/main/java/co/simplon/gamebotsback/persistance/entity/User.java
+++ src/main/java/co/simplon/gamebotsback/persistance/entity/User.java
@@ -1,4 +1,106 @@
 package co.simplon.gamebotsback.persistance.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "id_users")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsers;
+
+    @Basic
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "phone")
+    private int phone;
+
+    @Basic
+    @Column(name = "email")
+    private String email;
+
+    @Basic
+    @Column(name = "password")
+    private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modification_date")
+    private Date modificationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "id_games")
+    private Game games;
+
+    public int getIdUsers() {
+        return idUsers;
+    }
+
+    public void setIdUsers(int idUsers) {
+        this.idUsers = idUsers;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    public Game getGames() {
+        return games;
+    }
+
+    public void setGames(Game games) {
+        this.games = games;
+    }
 }
