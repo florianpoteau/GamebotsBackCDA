47c6222b8594a1990446805175a5c47a68f20088
YoanBor <yoanbor@icloud.com>
2024-04-04 14:10:47 +0200

    dev(build): Configuration initial du projet avec Spring data JPA, Spring boot et Spring Security

---
+++ src/main/java/co/simplon/gamebotsback/GamebotsBackApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.gamebotsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebotsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebotsBackApplication.class, args);
+	}
+
+}
