f423419e3f017ec1299a3b31d2f14c95c1aab912
YoanBor <yoanbor@icloud.com>
2024-04-04 16:07:02 +0200

    feat(game): Ajout de l'entity Game

--- src/main/java/co/simplon/gamebotsback/persistance/entity/Game.java
+++ src/main/java/co/simplon/gamebotsback/persistance/entity/Game.java
@@ -1,4 +1,129 @@
 package co.simplon.gamebotsback.persistance.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "games")
 public class Game {
+
+    @Id
+    @Column(name = "id_games")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idGames;
+
+    @Basic
+    @Column(name = "title")
+    private String title;
+
+    @Basic
+    @Column(name = "studio")
+    private String studio;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "release_date")
+    private Date releaseDate;
+
+    @Basic
+    @Column(name = "platform")
+    private String platform;
+
+    @Basic
+    @Column(name = "story")
+    private String story;
+
+    @Column(name = "isOnline")
+    private boolean isOnline;
+
+    @Column(name = "number_players")
+    private int numberPlayers;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modification_date")
+    private Date modificationDate;
+
+    public int getIdGames() {
+        return idGames;
+    }
+
+    public void setIdGames(int idGames) {
+        this.idGames = idGames;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    public int getNumberPlayers() {
+        return numberPlayers;
+    }
+
+    public void setNumberPlayers(int numberPlayers) {
+        this.numberPlayers = numberPlayers;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
 }
