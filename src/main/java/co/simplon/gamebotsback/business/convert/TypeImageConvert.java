53133f55cf025aef524ab263b305b12d29d12d1e
YoanBor <yoanbor@icloud.com>
2024-04-05 12:18:30 +0200

    feat(convert): Ajout du convert TypeImageConvert

---
+++ src/main/java/co/simplon/gamebotsback/business/convert/TypeImageConvert.java
@@ -0,0 +1,58 @@
+package co.simplon.gamebotsback.business.convert;
+
+import co.simplon.gamebotsback.business.dto.TypeImageDTO;
+import co.simplon.gamebotsback.persistance.entity.TypeImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeImageConvert {
+
+    private static TypeImageConvert instance;
+
+    private TypeImageConvert() {
+
+    }
+
+    public static TypeImageConvert getInstance() {
+        if (instance == null)
+            instance = new TypeImageConvert();
+        return instance;
+    }
+
+    public TypeImageDTO convertEntityToDto(final TypeImage entity) {
+        final TypeImageDTO dto = new TypeImageDTO();
+        dto.setIdTypeImage(entity.getIdTypeImage());
+        dto.setName(entity.getName());
+        dto.setCreationDate(entity.getCreationDate());
+        dto.setModificationDate(entity.getModificationDate());
+
+        return dto;
+    }
+
+    public TypeImage convertDtoToEntity(final TypeImageDTO dto) {
+        final TypeImage entity = new TypeImage();
+        entity.setIdTypeImage(dto.getIdTypeImage());
+        entity.setName(dto.getName());
+        entity.setCreationDate(dto.getCreationDate());
+        entity.setModificationDate(dto.getModificationDate());
+        return entity;
+    }
+
+    public List<TypeImage> convertDtoListToEntityList(final List<TypeImageDTO> dtoList) {
+        final List<TypeImage> entityList = new ArrayList<>();
+        for (TypeImageDTO dto : dtoList) {
+            entityList.add(convertDtoToEntity(dto));
+        }
+        return entityList;
+    }
+
+    public List<TypeImageDTO> convertEntityListToDtoList(final List<TypeImage> entityList) {
+        final List<TypeImageDTO> dtoList = new ArrayList<>();
+        for (TypeImage entity : entityList) {
+            dtoList.add(convertEntityToDto(entity));
+        }
+        return dtoList;
+    }
+}
+
