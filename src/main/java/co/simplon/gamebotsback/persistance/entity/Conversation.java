d619d943ba1d8ff8653a65348dfb00e8418dd67c
YoanBor <yoanbor@icloud.com>
2024-04-05 11:15:06 +0200

    fix(entity): Modification dans Conversation de l'id, de creationDate et de modificationDate. Rajout de user et de Game pour correspondre à la conception

--- src/main/java/co/simplon/gamebotsback/persistance/entity/Conversation.java
+++ src/main/java/co/simplon/gamebotsback/persistance/entity/Conversation.java
@@ -1,7 +1,6 @@
 package co.simplon.gamebotsback.persistance.entity;
 
 import jakarta.persistence.*;
-
 import java.util.Date;
 
 /**
@@ -12,84 +11,130 @@ import java.util.Date;
 @Table(name = "conversations")
 public class Conversation {
 
+    /** The unique identifier of the conversation. */
     @Id
-    @Column(name = "id_conversations")
+    @Column(name = "id_conversation")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idConversation;
 
+    /** The name of the conversation. */
     @Basic
     @Column(name = "name")
     private String name;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    /** The creation date of the conversation. */
+    @Temporal(TemporalType.DATE)
     @Column(name = "creation_date")
     private Date creationDate;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    /** The modification date of the conversation. */
+    @Temporal(TemporalType.DATE)
     @Column(name = "modification_date")
     private Date modificationDate;
 
+    /** The user associated with the conversation. */
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private User user;
+
+    /** The game associated with the conversation. */
+    @ManyToOne
+    @JoinColumn(name = "id_game")
+    private Game game;
+
     /**
-     * Retrieves the ID of the Conversation.
-     * @return The ID of the Conversation.
+     * Retrieves the ID of the conversation.
+     * @return The ID of the conversation.
      */
     public int getIdConversation() {
         return idConversation;
     }
 
     /**
-     * Sets the ID of the Conversation.
-     * @param idConversation The ID of the Conversation to set.
+     * Sets the ID of the conversation.
+     * @param idConversation The ID of the conversation to set.
      */
     public void setIdConversation(int idConversation) {
         this.idConversation = idConversation;
     }
 
     /**
-     * Retrieves the name of the Conversation.
-     * @return The name of the Conversation.
+     * Retrieves the name of the conversation.
+     * @return The name of the conversation.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Sets the name of the Conversation.
-     * @param name The name of the Conversation to set.
+     * Sets the name of the conversation.
+     * @param name The name of the conversation to set.
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * Retrieves the creation date of the Conversation.
-     * @return The creation date of the Conversation.
+     * Retrieves the creation date of the conversation.
+     * @return The creation date of the conversation.
      */
     public Date getCreationDate() {
         return creationDate;
     }
 
     /**
-     * Sets the creation date of the Conversation.
-     * @param creationDate The creation date of the Conversation to set.
+     * Sets the creation date of the conversation.
+     * @param creationDate The creation date of the conversation to set.
      */
     public void setCreationDate(Date creationDate) {
         this.creationDate = creationDate;
     }
 
     /**
-     * Retrieves the modification date of the Conversation.
-     * @return The modification date of the Conversation.
+     * Retrieves the modification date of the conversation.
+     * @return The modification date of the conversation.
      */
     public Date getModificationDate() {
         return modificationDate;
     }
 
     /**
-     * Sets the modification date of the Conversation.
-     * @param modificationDate The modification date of the Conversation to set.
+     * Sets the modification date of the conversation.
+     * @param modificationDate The modification date of the conversation to set.
      */
     public void setModificationDate(Date modificationDate) {
         this.modificationDate = modificationDate;
+    }
+
+    /**
+     * Retrieves the user associated with the conversation.
+     * @return The user associated with the conversation.
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the user associated with the conversation.
+     * @param user The user associated with the conversation to set.
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * Retrieves the game associated with the conversation.
+     * @return The game associated with the conversation.
+     */
+    public Game getGame() {
+        return game;
+    }
+
+    /**
+     * Sets the game associated with the conversation.
+     * @param game The game associated with the conversation to set.
+     */
+    public void setGame(Game game) {
+        this.game = game;
     }
 }
