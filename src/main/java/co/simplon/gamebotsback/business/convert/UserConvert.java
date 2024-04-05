bab0d0635c35a025e51065c1251b9a0791f84314
YoanBor <yoanbor@icloud.com>
2024-04-05 14:09:25 +0200

    feat(convert): Ajout du convert UserConvert

--- src/main/java/co/simplon/gamebotsback/business/convert/UserConvert.java
+++ src/main/java/co/simplon/gamebotsback/business/convert/UserConvert.java
@@ -3,6 +3,9 @@ package co.simplon.gamebotsback.business.convert;
 import co.simplon.gamebotsback.business.dto.UserDTO;
 import co.simplon.gamebotsback.persistance.entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserConvert {
 
     private static UserConvert instance;
@@ -26,6 +29,40 @@ public class UserConvert {
         dto.setPassword(entity.getPassword());
         dto.setCreationDate(entity.getCreationDate());
         dto.setModificationDate(entity.getModificationDate());
+        dto.setImage(entity.getImage());
+
         return dto;
+    }
+
+    public User convertDtoToEntity(final UserDTO dto) {
+        final User user = new User();
+        user.setIdUser(dto.getIdUser());
+        user.setUsername(dto.getUsername());
+        user.setPhone(dto.getPhone());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setCreationDate(dto.getCreationDate());
+        user.setModificationDate(dto.getModificationDate());
+        user.setImage(dto.getImage());
+
+        return user;
+    }
+
+    public List<User> convertDtoListToEntityList(final List<UserDTO> dtoList) {
+        final List<User> entityList = new ArrayList<>();
+        for (UserDTO dto : dtoList) {
+            entityList.add(convertDtoToEntity(dto));
+        }
+
+        return entityList;
+    }
+
+    public List<UserDTO> convertEntityListToDtoList(final List<User> entityList) {
+        final List<UserDTO> dtoList = new ArrayList<>();
+        for (User entity : entityList) {
+            dtoList.add(convertEntityToDto(entity));
+        }
+
+        return dtoList;
     }
 }
