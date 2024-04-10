c8076a36c804d6fe3e2cc8c9a918cdc083c789f3
Poteau Florian <florianpoteau59@gmail.com>
2024-04-10 14:35:03 +0200

    feat(Spring security): Ajout d'un Service permettant de décoder, générer, encoder, et authoriser des requêtes d'un utilisateur connecté

---
+++ src/main/java/co/simplon/gamebotsback/presentation/AuthController.java
@@ -0,0 +1,30 @@
+package co.simplon.gamebotsback.presentation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.gamebotsback.business.service.token.TokenService;
+
+@RestController
+public class AuthController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
+
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        LOG.debug("Token request for user: '{}", authentication.getName());
+        String token = tokenService.generateToken(authentication);
+        LOG.debug("Token granged {}", token);
+        return token;
+    }
+
+}
