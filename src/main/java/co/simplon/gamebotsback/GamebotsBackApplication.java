64774e94cd7c87fadc8bb198c080a5eae06add6c
Poteau Florian <florianpoteau59@gmail.com>
2024-04-10 14:49:49 +0200

    feat(Spring security): Ajout d'un Service permettant de décoder, générer, encoder, et authoriser des requêtes d'un utilisateur connecté

--- src/main/java/co/simplon/gamebotsback/GamebotsBackApplication.java
+++ src/main/java/co/simplon/gamebotsback/GamebotsBackApplication.java
@@ -6,17 +6,22 @@ package co.simplon.gamebotsback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import co.simplon.gamebotsback.config.RsaKeyProperties;
 
 /**
  * This class represents the main entry point for the GamebotsBack application.
  * It utilizes Spring Boot's {@code @SpringBootApplication} annotation to enable
  * auto-configuration and component scanning.
  */
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class GamebotsBackApplication {
 
 	/**
 	 * The main method to start the GamebotsBack application.
+	 * 
 	 * @param args Command-line arguments passed to the application.
 	 */
 	public static void main(String[] args) {
