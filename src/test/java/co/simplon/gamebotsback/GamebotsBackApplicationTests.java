a4465208ba8aa2be3631415b28c2260b60edac1f
YoanBor <yoanbor@icloud.com>
2024-04-16 11:55:16 +0200

    feat(github actions): Github actions CD pour déployer l'image sur docker

--- src/test/java/co/simplon/gamebotsback/GamebotsBackApplicationTests.java
+++ src/test/java/co/simplon/gamebotsback/GamebotsBackApplicationTests.java
@@ -1,8 +1,9 @@
 package co.simplon.gamebotsback;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GamebotsBackApplicationTests {
+
 
 }
