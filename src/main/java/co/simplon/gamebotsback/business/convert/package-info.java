b37360a46007fef11cad5f81bd9abbcaadefb97d
YoanBor <yoanbor@icloud.com>
2024-04-17 16:23:51 +0200

    refactor(): Suppression package.info et cd.yml

--- src/main/java/co/simplon/gamebotsback/business/convert/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Ce package fournit des classes pour la conversion entre les objets
- * ConversationDTO et les entités Conversation.
- */
-package co.simplon.gamebotsback.business.convert;
--- src/main/java/co/simplon/gamebotsback/business/dto/package-info.java
+++
@@ -1,1 +0,0 @@
-package co.simplon.gamebotsback.business.dto;
