8f92f9bd9a9a6f0d7ea603f1cb085df2f640e62d
Poteau Florian <florianpoteau59@gmail.com>
2024-04-18 11:19:53 +0200

    feat(Controller): ajout d'un controller pour récupérer la bannière d'un jeu

--- src/main/java/co/simplon/gamebotsback/presentation/controller/image/GetImageBannerByGameIdController.java
+++ src/main/java/co/simplon/gamebotsback/presentation/controller/image/GetImageBannerByGameIdController.java
@@ -2,7 +2,6 @@ package co.simplon.gamebotsback.presentation.controller.image;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +10,17 @@ import co.simplon.gamebotsback.business.dto.ImageDTO;
 import co.simplon.gamebotsback.business.service.image.IImageService;
 
 @RestController
-public class GetAllImagesByGameIdController {
+public class GetImageBannerByGameIdController {
 
-    private IImageService imageService;
+    private final IImageService imageService;
 
-    @Autowired
-    public GetAllImagesByGameIdController(IImageService imageService) {
+    public GetImageBannerByGameIdController(IImageService imageService) {
         this.imageService = imageService;
     }
 
-    @GetMapping("/games/{id}/images")
-    public List<ImageDTO> findAllImagesByGameId(@PathVariable int id) {
-        return imageService.getAllImagesByGameId("image_jeu", id);
+    @GetMapping("/games/{id}/banner")
+    List<ImageDTO> findImageBannerByGameId(@PathVariable int id) {
+        return imageService.getAllImagesByGameId("banner", id);
     }
 
 }
