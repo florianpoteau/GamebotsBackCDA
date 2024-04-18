f78f9a2ee264834b865247be11d2a7d1e860d9ea
Poteau Florian <florianpoteau59@gmail.com>
2024-04-18 11:03:35 +0200

    feat(Controller): ajout de controllers pour récupérer tous les images d'un jeu, ou récupérer l'avatar d'un utilisateur

---
+++ src/main/java/co/simplon/gamebotsback/presentation/controller/image/GetImageByUserIdController.java
@@ -0,0 +1,26 @@
+package co.simplon.gamebotsback.presentation.controller.image;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.gamebotsback.business.dto.ImageDTO;
+import co.simplon.gamebotsback.business.service.image.IImageService;
+
+@RestController
+public class GetImageByUserIdController {
+
+    private final IImageService imageService;
+
+    @Autowired
+    public GetImageByUserIdController(IImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @GetMapping("/users/{id}/images")
+    ImageDTO findImageByUserId(@PathVariable int id) {
+        return imageService.getImagesByUserIdAndImageType(id);
+    }
+
+}
