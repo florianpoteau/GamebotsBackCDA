211cbd08d9bb03f59663ea8bb3ce80c26903816a
YoanBor <yoanbor@icloud.com>
2024-04-20 04:07:09 +0200

    tests(ServicesTest): Ajout de tous les tests concernant les messages

---
+++ src/test/java/co/simplon/gamebotsback/unit/business/service/MessageServiceTest.java
@@ -0,0 +1,44 @@
+package co.simplon.gamebotsback.unit.business.service;
+
+import co.simplon.gamebotsback.business.dto.MessageDTO;
+import co.simplon.gamebotsback.business.service.message.MessageServiceImpl;
+import co.simplon.gamebotsback.persistance.entity.Message;
+import co.simplon.gamebotsback.persistance.repository.message.IMessageRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MessageServiceTest {
+
+    @InjectMocks
+    private MessageServiceImpl messageService;
+
+    @Mock
+    private IMessageRepository iMessageRepository;
+
+    @Test
+    @DisplayName("Test de récupération de tous les messages d'une conversation")
+    void getAllMessageConversation() {
+        when(iMessageRepository.getMessageByConversationId(any(Integer.class))).thenReturn(List.of(new Message()));
+        List<MessageDTO> messages = messageService.getAllMessageConversation(any(Integer.class));
+        verify(iMessageRepository, times(1)).getMessageByConversationId(any(Integer.class));
+        assertEquals(MessageDTO.class, messages.get(0).getClass(), "MessageDTO attendu dans la liste");
+    }
+
+    @Test
+    @DisplayName("Test d'ajout d'un nouveau message")
+    void addNewMessage() {
+        when(iMessageRepository.save(any(Message.class))).thenReturn(null);
+        messageService.addNewMessage(new MessageDTO());
+        verify(iMessageRepository, times(1)).save(any(Message.class));
+    }
+}
