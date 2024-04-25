a391b361833da16236ba507788ab428da032b748
YoanBor <yoanbor@icloud.com>
2024-04-25 16:27:06 +0200

    refactor(repository): Correction de structure pour correspondre à checkstyle - google checks - sun checks

--- src/main/java/co/simplon/gamebotsback/persistance/repository/image/Iimagerepository.java
+++ src/main/java/co/simplon/gamebotsback/persistance/repository/image/Iimagerepository.java
@@ -1,12 +1,3 @@
-/**
- * Repository interface for managing Image entities in the persistence layer.
- * This interface extends JpaRepository, providing access to basic CRUD (Create, Read, Update, Delete) operations
- * and additional methods for querying and manipulating Image entities.
- *
- * @param <Image>   The type of entity managed by this repository, which is Image.
- * @param <Integer> The type of the identifier property of the Image entity, which is Integer.
- * This represents the type of the primary key.
- */
 package co.simplon.gamebotsback.persistance.repository.image;
 
 import co.simplon.gamebotsback.persistance.entity.Image;
@@ -16,6 +7,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+/**
+ * Repository interface for managing Image entities in the persistence layer.
+ * This interface extends JpaRepository, providing access to basic CRUD (Create, Read, Update, Delete) operations
+ * and additional methods for querying and manipulating Image entities.
+ */
 public interface Iimagerepository extends JpaRepository<Image, Integer> {
 
   /**
