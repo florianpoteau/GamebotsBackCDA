caadcff89277debc60309a73683b4aac091e1222
Poteau Florian <florianpoteau59@gmail.com>
2024-04-29 13:23:36 +0200

    fix(spring security): requete permise pour se connecter et créer un compte

--- src/main/java/co/simplon/gamebotsback/config/SecurityConfig.java
+++ src/main/java/co/simplon/gamebotsback/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 return http
                                 .authorizeRequests(authorize -> authorize
-                                                .requestMatchers("/login").permitAll().anyRequest().authenticated()
+                                                .requestMatchers("/users", "/login").permitAll().anyRequest()
+                                                .authenticated()
 
                                 )
                                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
