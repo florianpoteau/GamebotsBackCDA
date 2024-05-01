8652f9f66816651004b0899944862733de1aa04b
YoanBor <yoanbor@icloud.com>
2024-05-01 21:54:27 +0200

    refactor, docs(): Ajout de javadoc et correction de structure pour correspondre à checkstyle - google checks - sun checks

--- src/main/java/co/simplon/gamebotsback/config/RsaKeyProperties.java
+++ src/main/java/co/simplon/gamebotsback/config/RsaKeyProperties.java
@@ -2,15 +2,20 @@ package co.simplon.gamebotsback.config;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * Represents RSA key properties used for JWT token encryption and decryption.
  * This class is used to load RSA public and private keys from configuration
  * properties.
+ *
+ * @param publicKey
+ *     The RSA public key usedfor token encryption and decryption.
+ * @param privateKey
+ *     The RSA private key used for token encryption and decryption.
  */
 @ConfigurationProperties(prefix = "rsa")
-public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+public record RsaKeyProperties(
+    RSAPublicKey publicKey, RSAPrivateKey privateKey) {
 
 }
