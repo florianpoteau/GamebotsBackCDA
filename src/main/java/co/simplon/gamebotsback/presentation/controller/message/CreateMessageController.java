85425528a06228f0386fd3ce00e7f11a9a882583
YoanBor <yoanbor@icloud.com>
2024-05-01 17:43:57 +0200

    refactor(controller): Correction de structure pour correspondre à checkstyle - google checks - sun checks

--- src/main/java/co/simplon/gamebotsback/presentation/controller/message/CreateMessageController.java
+++ src/main/java/co/simplon/gamebotsback/presentation/controller/message/CreateMessageController.java
@@ -1,41 +1,45 @@
-/**
- * Controller class for creating a new message in the presentation layer.
- * This controller handles HTTP POST requests to create a new message using the provided MessageDTO object.
- */
 package co.simplon.gamebotsback.presentation.controller.message;
 
+import co.simplon.gamebotsback.business.dto.Messagedto;
+import co.simplon.gamebotsback.business.service.message.Imessageservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.simplon.gamebotsback.business.dto.Messagedto;
-import co.simplon.gamebotsback.business.service.message.Imessageservice;
-
+/**
+ * Controller class for creating a new message in the presentation layer.
+ * This controller handles HTTP POST requests
+ * to create a new message using the provided MessageDTO object.
+ */
 @RestController
 public class CreateMessageController {
 
+  /**
+   * The service used to manage messages.
+   */
   private final Imessageservice messageService;
 
   /**
-   * Constructs a new CreateMessageController with the specified message service.
+   * Constructs a new CreateMessageController
+   * with the specified message service.
    *
-   * @param messageService The message service used to add a new message.
+   * @param serviceMessage The message service used to add a new message.
    */
   @Autowired
-  public CreateMessageController(Imessageservice messageService) {
-    this.messageService = messageService;
+  public CreateMessageController(final Imessageservice serviceMessage) {
+    this.messageService = serviceMessage;
   }
 
   /**
    * Handles HTTP POST requests to create a new message.
    *
-   * @param messageDTO The MessageDTO object containing the details of the message
-   *                   to be created.
+   * @param messageDto The MessageDTO object containing
+   *                   the details of the message to be created.
    */
   @PostMapping("/messages")
-  public void createMessage(@RequestBody Messagedto messageDTO) {
-    messageService.addNewMessage(messageDTO);
+  public void createMessage(@RequestBody final Messagedto messageDto) {
+    messageService.addNewMessage(messageDto);
   }
 
 }
