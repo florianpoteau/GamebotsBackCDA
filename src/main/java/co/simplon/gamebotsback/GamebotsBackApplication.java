0b78d940a8c0a023ee8d9d22638dd1cb874008a2
YoanBor <yoanbor@icloud.com>
2024-05-01 18:39:43 +0200

    refactor(config): Correction de structure pour correspondre à checkstyle - google checks - sun checks

--- src/main/java/co/simplon/gamebotsback/GamebotsBackApplication.java
+++ src/main/java/co/simplon/gamebotsback/GamebotsBackApplication.java
@@ -1,30 +1,28 @@
-/**
- * The main application class for the GamebotsBack application.
- * This class initializes and runs the Spring Boot application.
- */
 package co.simplon.gamebotsback;
 
+import co.simplon.gamebotsback.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import co.simplon.gamebotsback.config.RsaKeyProperties;
-
 /**
- * This class represents the main entry point for the GamebotsBack application.
- * It utilizes Spring Boot's {@code @SpringBootApplication} annotation to enable
+ * This class represents the main entry
+ * point for the GamebotsBack application.
+ * It utilizes Spring Boot's {@code @SpringBootApplication}
+ * annotation to enable
  * auto-configuration and component scanning.
  */
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class GamebotsBackApplication {
 
-	/**
-	 * The main method to start the GamebotsBack application.
-	 * 
-	 * @param args Command-line arguments passed to the application.
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(GamebotsBackApplication.class, args);
-	}
+  /**
+   * The main method to start the GamebotsBack application.
+   *
+   * @param args
+   *     Command-line arguments passed to the application.
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(GamebotsBackApplication.class, args);
+  }
 }
--- src/main/java/co/simplon/gamebotsback/config/SecurityConfig.java
+++ src/main/java/co/simplon/gamebotsback/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package co.simplon.gamebotsback.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +21,6 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
-
 /**
  * Configuration class for security settings.
  */
@@ -29,71 +28,80 @@ import com.nimbusds.jose.proc.SecurityContext;
 @EnableWebSecurity
 public class SecurityConfig {
 
-        private final RsaKeyProperties rsaKeys;
+  private final RsaKeyProperties rsaKeys;
 
-        /**
-         * Constructs a new SecurityConfig with the specified RSA key properties.
-         *
-         * @param rsaKeys the RSA key properties containing the public and private keys
-         */
-        @Autowired
-        public SecurityConfig(RsaKeyProperties rsaKeys) {
-                this.rsaKeys = rsaKeys;
-        }
+  /**
+   * Constructs a new SecurityConfig
+   * with the specified RSA key properties.
+   *
+   * @param keysRsa
+   *     the RSA key properties containing the public and private keys
+   */
+  @Autowired
+  public SecurityConfig(
+      final RsaKeyProperties keysRsa) {
+    this.rsaKeys = keysRsa;
+  }
 
-        /**
-         * Creates a PasswordEncoder bean.
-         *
-         * @return a BCryptPasswordEncoder instance
-         */
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return new BCryptPasswordEncoder();
-        }
+  /**
+   * Creates a PasswordEncoder bean.
+   *
+   * @return a BCryptPasswordEncoder instance
+   */
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-        /**
-         * Configures security filters for HTTP requests.
-         *
-         * @param http the HttpSecurity object to configure
-         * @return a SecurityFilterChain instance
-         * @throws Exception if an error occurs while configuring security
-         */
-        @SuppressWarnings({ "deprecation", "removal" })
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                return http
-                                .authorizeRequests(authorize -> authorize
-                                                .requestMatchers("/users", "/login").permitAll().anyRequest()
-                                                .authenticated()
+  /**
+   * Configures security filters for HTTP requests.
+   *
+   * @param http
+   *     the HttpSecurity object to configure
+   *
+   * @return a SecurityFilterChain instance
+   *
+   * @throws Exception
+   *     if an error occurs while configuring security
+   */
+  @SuppressWarnings({"deprecation", "removal"})
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+      final HttpSecurity http) throws Exception {
+    return http
+        .authorizeRequests(authorize -> authorize
+            .requestMatchers("/users", "/login").permitAll().anyRequest()
+            .authenticated()
 
-                                )
-                                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .logout().disable()
-                                .csrf().disable()
-                                .build();
-        }
+        )
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .logout().disable()
+        .csrf().disable()
+        .build();
+  }
 
-        /**
-         * Creates a JwtDecoder bean for decoding JWT tokens.
-         *
-         * @return a NimbusJwtDecoder instance
-         */
-        @Bean
-        JwtDecoder jwtDecoder() {
-                return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
-        }
+  /**
+   * Creates a JwtDecoder bean for decoding JWT tokens.
+   *
+   * @return a NimbusJwtDecoder instance
+   */
+  @Bean
+  JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+  }
 
-        /**
-         * Creates a JwtEncoder bean for encoding JWT tokens.
-         *
-         * @return a NimbusJwtEncoder instance
-         */
-        @Bean
-        JwtEncoder jwtEncoder() {
-                JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
-                JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-                return new NimbusJwtEncoder(jwks);
-        }
+  /**
+   * Creates a JwtEncoder bean for encoding JWT tokens.
+   *
+   * @return a NimbusJwtEncoder instance
+   */
+  @Bean
+  JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(
+        rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+    JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwks);
+  }
 }
