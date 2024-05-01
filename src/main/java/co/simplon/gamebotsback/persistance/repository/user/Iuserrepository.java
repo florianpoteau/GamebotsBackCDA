b96f1175a982ef9e25318666b83d9ff316f4b4f9
YoanBor <yoanbor@icloud.com>
2024-05-01 18:39:54 +0200

    refactor(persistance): Correction de structure pour correspondre à checkstyle - google checks - sun checks

--- src/main/java/co/simplon/gamebotsback/persistance/repository/user/Iuserrepository.java
+++ src/main/java/co/simplon/gamebotsback/persistance/repository/user/Iuserrepository.java
@@ -16,16 +16,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface Iuserrepository
-        extends JpaRepository<User, Integer> {
+    extends JpaRepository<User, Integer> {
 
-    /**
-     * Retrieves a user by username.
-     *
-     * @param username The username of the user.
-     * @return An optional containing the user
-     *         with the specified username, or empty if not found.
-     */
-    @Query(UserQueries.FIND_BY_USERNAME)
-    Optional<User> findByUsername(String username);
+  /**
+   * Retrieves a user by username.
+   *
+   * @param username The username of the user.
+   * @return An optional containing the user
+   * with the specified username, or empty if not found.
+   */
+  @Query(UserQueries.FIND_BY_USERNAME)
+  Optional<User> findByUsername(String username);
 
 }
