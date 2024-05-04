cb3c81ef4ac85ca0a93f9013e82080e558b903d8
Poteau Florian <florianpoteau59@gmail.com>
2024-05-04 11:12:09 +0200

    tests(RsaKeyProperties): test d'égalité entre deux instances disposant d'une clef privée différentes

--- src/test/java/co/simplon/gamebotsback/unit/config/RsaKeyPropertiesTest.java
+++ src/test/java/co/simplon/gamebotsback/unit/config/RsaKeyPropertiesTest.java
@@ -16,38 +16,46 @@ public class RsaKeyPropertiesTest {
 
     @Test
     public void testRsaKeyProperties() {
-        // Création de clés RSA factices pour les tests
         RSAPublicKey mockPublicKey = mock(RSAPublicKey.class);
         RSAPrivateKey mockPrivateKey = mock(RSAPrivateKey.class);
 
-        // Création d'une instance de RsaKeyProperties
         RsaKeyProperties rsaKeyProperties = new RsaKeyProperties(mockPublicKey, mockPrivateKey);
 
-        // Vérification que les clés sont correctement attribuées
         assertEquals(mockPublicKey, rsaKeyProperties.publicKey());
         assertEquals(mockPrivateKey, rsaKeyProperties.privateKey());
     }
 
-    // Teste l'égalité entre les instances de RsaKeyProperties.
     @Test
-    public void testEquality() {
-        // Création de clés RSA factices pour les tests
+    public void testEqualityPublicKey() {
         RSAPublicKey mockPublicKey = mock(RSAPublicKey.class);
         RSAPrivateKey mockPrivateKey = mock(RSAPrivateKey.class);
 
-        // Création de deux instances avec les mêmes clés
         RsaKeyProperties rsaKeyProperties1 = new RsaKeyProperties(mockPublicKey, mockPrivateKey);
         RsaKeyProperties rsaKeyProperties2 = new RsaKeyProperties(mockPublicKey, mockPrivateKey);
 
-        // Vérification que les deux instances sont égales
         assertEquals(rsaKeyProperties1, rsaKeyProperties2);
 
-        // Modification d'une des clés dans la deuxième instance
         RSAPublicKey mockPublicKey2 = mock(RSAPublicKey.class);
         when(mockPublicKey2.getAlgorithm()).thenReturn("RSA");
         RsaKeyProperties rsaKeyProperties3 = new RsaKeyProperties(mockPublicKey2, mockPrivateKey);
 
-        // Vérification que les instances ne sont plus égales
+        assertNotEquals(rsaKeyProperties1, rsaKeyProperties3);
+    }
+
+    @Test
+    public void testEqualityPrivateKey() {
+        RSAPublicKey mockPublicKey = mock(RSAPublicKey.class);
+        RSAPrivateKey mockPrivateKey = mock(RSAPrivateKey.class);
+
+        RsaKeyProperties rsaKeyProperties1 = new RsaKeyProperties(mockPublicKey, mockPrivateKey);
+        RsaKeyProperties rsaKeyProperties2 = new RsaKeyProperties(mockPublicKey, mockPrivateKey);
+
+        assertEquals(rsaKeyProperties1, rsaKeyProperties2);
+
+        RSAPrivateKey mockPrivateKey2 = mock(RSAPrivateKey.class);
+        when(mockPrivateKey2.getFormat()).thenReturn("PKCS#8");
+        RsaKeyProperties rsaKeyProperties3 = new RsaKeyProperties(mockPublicKey, mockPrivateKey2);
+
         assertNotEquals(rsaKeyProperties1, rsaKeyProperties3);
     }
 
