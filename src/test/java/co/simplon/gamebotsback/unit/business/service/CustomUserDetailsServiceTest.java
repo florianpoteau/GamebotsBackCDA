4e52a7bb46abc9de8c1786b1e2770ab8e4694910
Poteau Florian <florianpoteau59@gmail.com>
2024-05-05 14:20:44 +0200

    tests(CustomUserDetailsService): test permettant de charger les données de l'utilisateur à partir de la base de donnée simulée

---
+++ src/test/java/co/simplon/gamebotsback/unit/business/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,66 @@
+package co.simplon.gamebotsback.unit.business.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import co.simplon.gamebotsback.business.service.token.CustomUserDetailsService;
+
+public class CustomUserDetailsServiceTest {
+
+    @Mock
+    private DataSource dataSource;
+
+    @Mock
+    private ResultSet resultSet;
+
+    @Mock
+    private PreparedStatement preparedStatement;
+
+    @Mock
+    private Connection connection;
+
+    private CustomUserDetailsService customUserDetailsService;
+
+    String username = "username";
+    String password = "password";
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        customUserDetailsService = new CustomUserDetailsService(dataSource);
+    }
+
+    @Test
+    void testLoadUserByUsername() throws SQLException {
+
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString("password")).thenReturn(password);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(connection.prepareStatement(any())).thenReturn(preparedStatement);
+        when(dataSource.getConnection()).thenReturn(connection);
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        assertEquals(username, userDetails.getUsername());
+        assertEquals(password, userDetails.getPassword());
+        verify(connection).prepareStatement(any());
+        verify(preparedStatement).setString(1, username);
+
+    }
+
+}
