ea0abe285993fc152a78e4b3a1bde6ad756ccd65
Poteau Florian <florianpoteau59@gmail.com>
2024-05-06 10:47:47 +0200

    tests(AuthController): ajout du test unitaire qui test une connexion qui a échoué

--- src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/token/AuthControllerTest.java
+++ src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/token/AuthControllerTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,5 +47,22 @@ public class AuthControllerTest {
 
         verify(userService, times(1)).getIdByUsername(userDto.getUsername());
         verify(tokenService, times(1)).generateToken(userDto.getUsername(), userDto.getPassword(), 1);
+    }
+
+    @Test
+    void testLoginFailure() {
+
+        Userdto userDto = new Userdto();
+        userDto.setUsername(null);
+        userDto.setPassword(null);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> authController.login(userDto));
+
+        assertEquals("Authentication object is null or does not contain a name", exception.getMessage());
+
+        verify(userService, never()).getIdByUsername(anyString());
+        verify(tokenService, never()).generateToken(anyString(), anyString(), anyInt());
+
     }
 }
