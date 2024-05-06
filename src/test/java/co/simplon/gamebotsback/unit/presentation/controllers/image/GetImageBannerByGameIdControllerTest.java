b7df93ee10331a37741f331c7f97c349a2d413d5
YoanBor <yoanbor@icloud.com>
2024-05-06 17:32:25 +0200

    Tests(GetImageBannerByGameIdControllerTest): Ajout du test GetImageBannerByGameIdControllerTest pour GetImageBannerByGameIdController

---
+++ src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/image/GetImageBannerByGameIdControllerTest.java
@@ -0,0 +1,31 @@
+package co.simplon.gamebotsback.unit.presentation.controllers.image;
+
+import co.simplon.gamebotsback.business.dto.Imagedto;
+import co.simplon.gamebotsback.business.service.image.Iimageservice;
+import co.simplon.gamebotsback.presentation.controller.image.GetImageBannerByGameIdController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetImageBannerByGameIdControllerTest {
+
+  @InjectMocks
+  private GetImageBannerByGameIdController controller;
+
+  @Mock
+  private Iimageservice imageService;
+
+  @Test
+  void getImageBannerByGameId() {
+    when(imageService.getAllImagesByGameIdAndImageType("banner", 1)).thenReturn(List.of(new Imagedto()));
+    controller.findImageBannerByGameId(1);
+    verify(imageService, times(1)).getAllImagesByGameIdAndImageType("banner", 1);
+  }
+}
