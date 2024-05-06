48c641e83f9272202c17726436dd9a96b6e509e1
YoanBor <yoanbor@icloud.com>
2024-05-06 14:43:41 +0200

    Tests(GetAllImagesByGameIdControllerTest): test unitaire pour GetAllImagesByGameIdController

---
+++ src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/image/GetAllImagesByGameIdControllerTest.java
@@ -0,0 +1,31 @@
+package co.simplon.gamebotsback.unit.presentation.controllers.image;
+
+import co.simplon.gamebotsback.business.dto.Imagedto;
+import co.simplon.gamebotsback.business.service.image.Iimageservice;
+import co.simplon.gamebotsback.presentation.controller.image.GetAllImagesByGameIdController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetAllImagesByGameIdControllerTest {
+
+  @InjectMocks
+  private GetAllImagesByGameIdController controller;
+
+  @Mock
+  private Iimageservice imageService;
+
+  @Test
+  void getAllImagesByGameId() {
+    when(imageService.getAllImagesByGameIdAndImageType("image_jeu", 1)).thenReturn(List.of(new Imagedto()));
+    controller.findAllImagesByGameId(1);
+    verify(imageService, times(1)).getAllImagesByGameIdAndImageType("image_jeu", 1);
+  }
+}
