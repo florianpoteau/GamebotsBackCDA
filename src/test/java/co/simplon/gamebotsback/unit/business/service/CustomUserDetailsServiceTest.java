00ffe92009337e08d95ca82de3a7f2bddd3ab296
Poteau Florian <florianpoteau59@gmail.com>
2024-05-06 10:41:52 +0200

    tests(AuthController): ajout du test unitaire pour tester une connexion réussi d'un utilisateur

--- src/test/java/co/simplon/gamebotsback/unit/business/service/CustomUserDetailsServiceTest.java
+++ src/test/java/co/simplon/gamebotsback/unit/business/service/CustomUserDetailsServiceTest.java
@@ -3,7 +3,6 @@ package co.simplon.gamebotsback.unit.business.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
---
+++ src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/token/AuthControllerTest.java
@@ -0,0 +1,48 @@
+package co.simplon.gamebotsback.unit.presentation.controllers.token;
+
+import co.simplon.gamebotsback.business.dto.Userdto;
+import co.simplon.gamebotsback.business.service.token.TokenService;
+import co.simplon.gamebotsback.business.service.user.Iuserservice;
+import co.simplon.gamebotsback.presentation.controller.token.AuthController;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthControllerTest {
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
+    private Iuserservice userService;
+
+    @InjectMocks
+    private AuthController authController;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        Userdto userDto = new Userdto();
+        userDto.setUsername("username");
+        userDto.setPassword("password");
+
+        when(userService.getIdByUsername(userDto.getUsername())).thenReturn(1);
+        when(tokenService.generateToken(userDto.getUsername(), userDto.getPassword(), 1)).thenReturn("generatedToken");
+
+        String token = authController.login(userDto);
+
+        assertNotNull(token);
+
+        verify(userService, times(1)).getIdByUsername(userDto.getUsername());
+        verify(tokenService, times(1)).generateToken(userDto.getUsername(), userDto.getPassword(), 1);
+    }
+}
