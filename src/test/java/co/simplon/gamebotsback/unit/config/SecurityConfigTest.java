4a81f5b456b05dda8cff426064f8008394976349
Poteau Florian <florianpoteau59@gmail.com>
2024-05-07 10:35:29 +0200

    tests(SecurityConfig): ajout du test JwtDecoder

--- src/test/java/co/simplon/gamebotsback/unit/config/SecurityConfigTest.java
+++ src/test/java/co/simplon/gamebotsback/unit/config/SecurityConfigTest.java
@@ -1,9 +1,13 @@
 package co.simplon.gamebotsback.unit.config;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import co.simplon.gamebotsback.config.RsaKeyProperties;
 import co.simplon.gamebotsback.config.SecurityConfig;
@@ -19,21 +25,30 @@ import co.simplon.gamebotsback.config.SecurityConfig;
 class SecurityConfigTest {
 
   @Mock
-  PasswordEncoder passwordEncoder;
+  private PasswordEncoder passwordEncoder;
 
   @Mock
-  RsaKeyProperties rsaKeyProperties;
+  private RsaKeyProperties rsaKeyProperties;
+
+  @Mock
+  private RSAPublicKey rsaPublicKey;
+
+  @Mock
+  private RSAPrivateKey rsaPrivateKey;
 
   @InjectMocks
-  SecurityConfig securityConfig;
+  private SecurityConfig securityConfig;
 
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
+    when(rsaKeyProperties.publicKey()).thenReturn(rsaPublicKey);
+    when(rsaKeyProperties.privateKey()).thenReturn(rsaPrivateKey);
   }
 
   @Test
   void testPasswordEncoder() {
+
     SecurityConfig securityConfig = new SecurityConfig(rsaKeyProperties);
 
     PasswordEncoder encoder = securityConfig.passwordEncoder();
@@ -45,6 +60,7 @@ class SecurityConfigTest {
 
   @Test
   void testPasswordDecoder() {
+
     String plainPassword = "password";
 
     String encodedPassword = "$2a$123";
@@ -58,10 +74,26 @@ class SecurityConfigTest {
 
   @Test
   void testAutowiredRsaKeyProperties() {
+
     SecurityConfig securityConfig = new SecurityConfig(rsaKeyProperties);
 
     assertNotNull(securityConfig);
 
+  }
+
+  @Test
+  void testJwtDecoder() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+      IllegalArgumentException, InvocationTargetException {
+
+    SecurityConfig securityConfig = new SecurityConfig(rsaKeyProperties);
+
+    Method method = SecurityConfig.class.getDeclaredMethod("jwtDecoder");
+    method.setAccessible(true);
+
+    JwtDecoder decoder = (JwtDecoder) method.invoke(securityConfig);
+
+    assertNotNull(decoder);
+    assertTrue(decoder instanceof NimbusJwtDecoder);
   }
 
 }
