915a235930c8bc12e2dbf751a390450c2a392c5c
YoanBor <yoanbor@icloud.com>
2024-05-11 07:57:08 +0200

    refactor(Image, User): changement 'GetImageByIdControllerTest' en 'GetImageByImageIdControllerTest' et 'GetUserAccountByIdControllerTest' en 'GetUserAccountByUserIdControllerTest'

--- src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/user/GetUserAccountByUserIdControllerTest.java
+++ src/test/java/co/simplon/gamebotsback/unit/presentation/controllers/user/GetUserAccountByUserIdControllerTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class GetUserAccountByIdControllerTest {
+class GetUserAccountByUserIdControllerTest {
 
   @InjectMocks
   GetUserAccountByUserIdController controller;
